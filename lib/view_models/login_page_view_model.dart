f4d568fe640f011d9c7afee71eb665d2f14ab09f
Mitsuhiro Komuro <mitsuhiro.komuro@gmail.com>
2021-04-29 14:53:55 +0000

    Update  page design of login page

--- lib/view_models/login_page_view_model.dart
+++ lib/view_models/login_page_view_model.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tms/controllers/authentication_contorller.dart';
 import 'package:tms/providers/login_page_provider.dart';
 
-class LoginPageViewModel {
+class LoginPageViewModel extends ChangeNotifier {
+  final _formKey = GlobalKey<FormState>();
 
+  get formKey => _formKey;
+
+  /// Update Email prvider state with user inputed [email] address at called [context].
   void updateEmail(BuildContext context, String email) {
     context.read(emailProvider).state = email;
   }
 
-  void updatePassword(BuildContext context, String pass) {
-    context.read(passwordProvider).state = pass;
+  /// Update Password prvider state with user inputed [password] at called [context].
+  void updatePassword(BuildContext context, String password) {
+    context.read(passwordProvider).state = password;
+  }
+
+  /// Sign in to the system with valid formated [email] address and [password].
+  void signIn(AuthenticationContorller _auth, String email, String password) {
+    if (this._formKey.currentState!.validate()) {
+      this._formKey.currentState!.save();
+      _auth.signIn(email: email, password: password);
+    }
   }
 }
--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -4,44 +4,111 @@ import 'package:tms/controllers/authentication_contorller.dart';
 import 'package:tms/providers/auth_provider.dart';
 import 'package:tms/providers/login_page_provider.dart';
 import 'package:tms/view_models/login_page_view_model.dart';
+import 'package:tms/res/strings/login.dart';
 
 class LoginPage extends ConsumerWidget {
-  final _viewModel = LoginPageViewModel();
+  final LoginPageViewModel _viewModel = LoginPageViewModel();
 
   @override
   Widget build(BuildContext context, ScopedReader watch) {
     final String email = watch(emailProvider).state;
-    final String pass = watch(passwordProvider).state;
+    final String password = watch(passwordProvider).state;
     final AuthenticationContorller _auth = watch(authServicesProvider);
 
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Login',
-            style: TextStyle(
-              fontSize: 24,
+      body: Form(
+        key: _viewModel.formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Text(
+                welcomeMessage,
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
             ),
-          ),
-          TextField(
-            onChanged: (value) => _viewModel.updateEmail(context, value),
-            decoration: InputDecoration(
-              hintText: 'Email',
+            Container(
+              child: TextFormField(
+                // onChanged: (value) => _viewModel.updateEmail(context, value),
+                decoration: InputDecoration(
+                  hintText: emailHintText,
+                  border: OutlineInputBorder(),
+                ),
+                validator: (String? value) {
+                  if (value != null && value.isEmpty) {
+                    return emailValidateMsg;
+                  } else if (value != null && value.contains('@') == false) {
+                    return passwordFormatValidateMsg;
+                  } else {
+                    return null;
+                  }
+                },
+                onSaved: (String? value) {
+                  if (value != null) {
+                    _viewModel.updateEmail(context, value);
+                  }
+                },
+              ),
+              padding: EdgeInsets.fromLTRB(50, 20, 50, 0),
+              alignment: Alignment.center,
             ),
-          ),
-          TextField(
-            onChanged: (value) => _viewModel.updatePassword(context, value),
-            obscureText: true,
-            decoration: InputDecoration(
-              hintText: 'Password',
+            Container(
+              child: TextFormField(
+                // onChanged: (value) => _viewModel.updatePassword(context, value),
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: passwordHintText,
+                  border: OutlineInputBorder(),
+                ),
+                validator: (String? value) {
+                  if (value != null && value.isEmpty) {
+                    return passWordValidateMsg;
+                  } else {
+                    return null;
+                  }
+                },
+                onSaved: (String? value) {
+                  if (value != null) {
+                    _viewModel.updatePassword(context, value);
+                  }
+                },
+              ),
+              padding: EdgeInsets.fromLTRB(50, 20, 50, 20),
+              alignment: Alignment.center,
             ),
-          ),
-          ElevatedButton(
-            onPressed: () => _auth.signIn(email: email, password: pass),
-            child: Text('Sign-in'),
-          ),
-        ],
+            Container(
+              child: ElevatedButton(
+                onPressed: () => _viewModel.signIn(_auth, email, password),
+                child: Text(
+                  loginButtonLabel,
+                ),
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                ),
+              ),
+              padding: EdgeInsets.fromLTRB(50, 20, 50, 5),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: OutlinedButton(
+                onPressed: () => _auth.signUp(email: email, password: password),
+                child: Text(
+                  signUpButtonLabel,
+                  style: TextStyle(
+                    color: Colors.blue,
+                  ),
+                ),
+                style: OutlinedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                  side: BorderSide(width: 1, color: Colors.blue),
+                ),
+              ),
+              padding: EdgeInsets.fromLTRB(50, 5, 50, 20),
+            ),
+          ],
+        ),
       ),
     );
   }
